780a2fe22104cf3e87501c2f321f60d314dfdad0
Kenyansa <kenyansajustine@gmail.com>
2022-04-08 13:05:02 +0300

    compare string '10' and 10 and equate the string to the number

--- testsOnArraysAndFunctions.js
+++ testsOnArraysAndFunctions.js
@@ -63,3 +63,8 @@ console.log(par);
 //printing mother teresa's quote
 const mteresa = "Love is not patronizing and charity isn't about pity, it is about love.\n Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.";
 console.log(mteresa);
+//comparing datatypes
+let a = "10";
+let b = 10;
+console.log(typeof a === b ? "True" : parseInt(a));
+console.log(a);
