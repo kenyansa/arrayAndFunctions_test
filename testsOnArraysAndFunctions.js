30fdd8285e51842770278f776447edf657dc557e
Kenyansa <kenyansajustine@gmail.com>
2022-04-08 08:40:50 +0300

    create a string called challenge and print it out. Also print its length

---
+++ testsOnArraysAndFunctions.js
@@ -0,0 +1,30 @@
+// Exercise: Level 1
+// Start of the Assignment
+// Declare a variable named challenge and assign it to an initial value 'Moringa is great'.
+let challenge = 'Moringa is great'; 
+// Print the string on the browser console using console.log()
+console.log(challenge);
+// Print the length of the string on the browser console using console.log()
+console.log(challenge.length);
+// Change all the string characters to capital letters using toUpperCase() method
+// Change all the string characters to lowercase letters using toLowerCase() method
+// Cut (slice) out the first word of the string using substr() or substring() method
+// Slice out the phrase Days Of JavaScript from Moringa is great.
+// Check if the string contains a word Script using includes() method
+// Split the string into an array using split() method
+// Split the string Moringa is great at the space using split() method
+// 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+// Change Moringa is great to 30 Days Of Python using replace() method.
+// What is character at index 15 in 'Moringa is great' string? Use charAt() method.
+// What is the character code of J in 'Moringa is great' string using charCodeAt()
+// Use indexOf to determine the position of the first occurrence of a in Moringa is great
+// Use lastIndexOf to determine the position of the last occurrence of a in Moringa is great.
+// Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+// Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' Moringa is great '.
+// Use startsWith() method with the string Moringa is great and make the result true
+// Use endsWith() method with the string Moringa is great and make the result true
+// Use match() method to find all the a’s in Moringa is great
+// Use concat() and merge '30 Days of' and 'JavaScript' to a single string, 'Moringa is great'
+// Use repeat() method to print Moringa is great 2 times
